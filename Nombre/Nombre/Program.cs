1356b528803a9e5681ad9c671b8154b704d5aa0b
Heba Omar <74996096+Heba-WebDev@users.noreply.github.com>
2022-12-08 04:14:45 +0300

    Ejercicio 1
    Escribe un programa que reciba tu nombre y lo escriba por consola.

---
+++ Nombre/Nombre/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string? name;
+Console.Write("Pleas enter your name ");
+name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}!");
+Console.ReadLine();
+
