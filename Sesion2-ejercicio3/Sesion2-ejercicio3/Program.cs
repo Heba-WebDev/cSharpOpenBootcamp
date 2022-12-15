913103f6a576a1d96777a405da7d9aa4d1d068de
Heba Omar <74996096+Heba-WebDev@users.noreply.github.com>
2022-12-15 03:59:05 +0300

    Sesión 2 - ejercicio 3
    Operadores Determina los operadores para verificar las siguientes condiciones:
    
    Un número es mayor o igual a 18
    
    Un char es ‘a’
    
    Se cumplen dos conciones a la vez (ambas verdaderas)
    
    Se cumple una de dos condiciones a la vez (una true y otra false)

---
+++ Sesion2-ejercicio3/Sesion2-ejercicio3/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int number;
+
+Console.Write("Pleae enter a number ");
+
+number = System.Convert.ToInt32(Console.ReadLine());
+
+char letter;
+
+Console.Write("Pleae enter a letter ");
+
+letter = Console.ReadLine()[0];
+
+
+if(number > 18)
+{
+    Console.WriteLine("The number you entered is bigger than 18 ");
+}else
+{
+    Console.WriteLine("The number you entered is smaller than 18 ");
+}
+
+if(letter == 'a')
+{
+    Console.WriteLine("The letter you wrote is a");
+} else
+{
+    Console.WriteLine("The letter you wrote isn't a");
+}
+
+if(number < 18 && letter == 'a')
+{
+    Console.WriteLine($"both conditions are {true}");
+} else
+{
+    Console.WriteLine($"both conditions are not {true}");
+}
+
+Console.ReadLine();
