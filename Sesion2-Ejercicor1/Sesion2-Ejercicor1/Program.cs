8b04c7632b891cdb361bbfed089846f46eb294f7
Heba Omar <74996096+Heba-WebDev@users.noreply.github.com>
2022-12-15 03:49:16 +0300

    Sesión 2 - ejercicio 1
    Variables Escribe un programa que reciba datos de una persona y genera un mensaje, usa una variable para cada dato y otra para el mensaje. Ej: nombre, apellido, edad, sabe programar, etc.

---
+++ Sesion2-Ejercicor1/Sesion2-Ejercicor1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string? nombre;
+Console.Write("Nombre? ");
+nombre = Console.ReadLine();
+
+string? apellido;
+Console.Write("Apellido? ");
+apellido = Console.ReadLine();
+
+int? edad;
+Console.Write("Edad? ");
+edad = System.Convert.ToInt32(Console.ReadLine());
+
+string? sabeProgramar;
+Console.Write("Usted sabe programar? ");
+sabeProgramar = Console.ReadLine();
+
+
+Console.WriteLine($"Se llama {nombre}, su apellido es {apellido}, tiene usted {edad} y usted {sabeProgramar} sabe programar ");
+
+Console.ReadLine();
